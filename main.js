a83cde4d3cd42370495bf2d3a8ca2a5270345145
Joanna Matanga <145405104+jomatanga@users.noreply.github.com>
2024-01-11 19:53:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+console.log("Hello World")
+
+const answer = document.querySelector(".display")
+console.log(answer)
+
+let one = document.querySelector("#one")
+one.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let two = document.querySelector("#two")
+two.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let three = document.querySelector("#three")
+three.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let four = document.querySelector("#four")
+four.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let five = document.querySelector("#five")
+five.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let six = document.querySelector("#six")
+six.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let seven = document.querySelector("#seven")
+seven.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let eight = document.querySelector("#eight")
+eight.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let nine = document.querySelector("#nine")
+nine.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let zero = document.querySelector("#zero")
+zero.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let doublezero = document.querySelector("#doublezero")
+doublezero.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let minus = document.querySelector("#minus")
+minus.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let multiply = document.querySelector("#multiply")
+multiply.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let plus = document.querySelector("#plus")
+plus.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
+
+let divide = document.querySelector("#divide")
+divide.addEventListener("click", e => {
+  answer.innerText += e.target.value  
+})
